0e6058a829518fe176ff3409d7cab43d96e47c84
PikachuHy <pikachuhy@163.com>
2021-03-09 21:28:01 +0800

    添加Toast组件

---
+++ Toast.h
@@ -0,0 +1,50 @@
+//
+// Created by PikachuHy on 2021/3/9.
+//
+/*
+background-color: rgba(0,0,0,0.80);
+border-radius: 26px;
+color: #FFFFFF;
+font-family: microsoft yahei;
+font-size: 16px;
+padding-left:25px;
+padding-right:25px;
+ */
+#ifndef MYNOTES_TOAST_H
+#define MYNOTES_TOAST_H
+
+#include <QFrame>
+#include <QLabel>
+#include <QIcon>
+#include <QGraphicsOpacityEffect>
+#include <QPropertyAnimation>
+#include <QPushButton>
+
+#include <QHBoxLayout>
+#include "ui_Toast.h"
+
+class Toast : public QWidget
+{
+Q_OBJECT
+
+public:
+    Toast(QWidget *parent = Q_NULLPTR);
+    ~Toast();
+
+    void setText(const QString& text);
+
+    void showAnimation(int timeout = 2000);// 动画方式show出，默认2秒后消失
+
+public:
+    // 静态调用
+    static void showTip(const QString& text, QWidget* parent = nullptr);
+
+protected:
+    virtual void paintEvent(QPaintEvent *event);
+
+private:
+    Ui::ToastClass ui;
+};
+
+
+#endif //MYNOTES_TOAST_H
