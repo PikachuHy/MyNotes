91e2996ba309fc222d1fa28ee56bddd393e4a4cd
PikachuHy <pikachuhy@163.com>
2021-03-25 10:39:24 +0800

    暂时不那么可以用查询API

--- ElasticSearchRestApi.cpp
+++ ElasticSearchRestApi.cpp
@@ -15,13 +15,14 @@
 #include <QDebug>
 #include <QEventLoop>
 #include <QObject>
+#include <QJsonArray>
 void ElasticSearchRestApi::putNote(const QString &owner, const QString &noteHtml, const Note& note) {
 
     QNetworkAccessManager* manager = new QNetworkAccessManager;
     QNetworkRequest request;
     QByteArray line;
     QEventLoop eventLoop;
-    QString url = QString("http://192.168.3.100:9200/note/_doc/") + note.strId();
+    QString url = baseUrl + QString("/note/_doc/") + note.strId();
     request.setUrl(QUrl(url));
     request.setHeader(QNetworkRequest::ContentTypeHeader,"application/json");
     QJsonObject jsonObject;
@@ -82,4 +83,85 @@ void ElasticSearchRestApi::handleResult(const QByteArray &ret) {
         }
     }
 
+}
+
+QList<SearchResult> ElasticSearchRestApi::search(const QString &q) {
+    QList<SearchResult> ret;
+
+    QNetworkAccessManager* manager = new QNetworkAccessManager;
+    QNetworkRequest request;
+    QByteArray line;
+    QEventLoop eventLoop;
+    QString url = baseUrl + QString("/note/_search");
+    request.setUrl(QUrl(url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader,"application/json");
+
+    QByteArray append = buildSearchJson(q);
+//    QByteArray append(R"({"user":"david","token":"e10adc3949ba59abbe56e057f20f883e"})");
+    qDebug() << url;
+    qDebug().noquote() << append;
+    // 强行POST
+    QNetworkReply* reply = manager->post(request, append);
+    QObject::connect(manager, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+    eventLoop.exec();
+    line=reply->readAll();
+
+    qDebug().noquote() << line;
+    QJsonParseError jsonError;
+// 转化为 JSON 文档
+    QJsonDocument doucment = QJsonDocument::fromJson(line, &jsonError);
+// 解析未发生错误
+    if (!doucment.isNull() && (jsonError.error == QJsonParseError::NoError))
+    {
+        if (doucment.isObject())
+        {
+            // JSON 文档为对象
+            QJsonObject object = doucment.object();  // 转化为对象
+            if (object.contains("_shards")) {
+                auto shardsObject = object.value("_shards").toObject();
+                // 包含指定的 key
+                QJsonValue value = shardsObject.value("successful");  // 获取指定 key 对应的 value
+                qDebug() << value;
+                if (value.toInt()) {
+                    qDebug() << "search success";
+                } else {
+                    qDebug() << "search fail";
+                }
+            }
+            if (object.contains("hits")) {
+                // TODO 构造查询结果
+            }
+        }
+    }
+    return ret;
+}
+
+ElasticSearchRestApi::ElasticSearchRestApi(QObject *parent) : QObject(parent) {
+    baseUrl = "http://192.168.3.100:9200";
+}
+
+QByteArray ElasticSearchRestApi::buildSearchJson(const QString& q) {
+    QJsonObject jsonObject;
+    QJsonObject matchObject;
+    matchObject.insert("note", q);
+    QJsonObject queryObject;
+    queryObject.insert("match", matchObject);
+    jsonObject.insert("query", queryObject);
+    QJsonObject highlightObject;
+    highlightObject.insert("pre_tags", "<span style='color:red'>");
+    highlightObject.insert("post_tags", "</span>");
+    QJsonObject fieldsObject;
+    QJsonObject noteObject;
+    fieldsObject.insert("note", noteObject);
+    highlightObject.insert("fields", fieldsObject);
+    jsonObject.insert("highlight", highlightObject);
+    QJsonArray sourceArray;
+    sourceArray.append("title");
+    sourceArray.append("note");
+    jsonObject.insert("_source", sourceArray);
+    jsonObject.insert("from", 0);
+    jsonObject.insert("size", 10);
+    QJsonDocument document;
+    document.setObject(jsonObject);
+    return document.toJson(QJsonDocument::Compact);
 }
--- ElasticSearchRestApi.h
+++ ElasticSearchRestApi.h
@@ -8,12 +8,21 @@
 #include <QString>
 #include <QObject>
 #include "DbModel.h"
+struct SearchResult {
+    QString strId;
+    QString note;
+    QString highlight;
+};
 class ElasticSearchRestApi: public QObject {
     Q_OBJECT
 public:
-    explicit ElasticSearchRestApi(QObject* parent = nullptr): QObject(parent) {}
+    explicit ElasticSearchRestApi(QObject* parent = nullptr);
     void putNote(const QString &owner, const QString &noteHtml, const Note& note);
     void handleResult(const QByteArray& ret);
+    QList<SearchResult> search(const QString& q);
+    QByteArray buildSearchJson(const QString& q);
+private:
+    QString baseUrl;
 };
 
 
