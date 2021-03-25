868204665f923a79fd3f22a2120d6a4618670d4c
PikachuHy <pikachuhy@163.com>
2021-03-25 21:42:37 +0800

    更新翻译

--- MyNotes_zh_CN.ts
+++ MyNotes_zh_CN.ts
@@ -1,13 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="MyNotes_zh_CN">
+<TS version="2.1" language="zh_CN">
 <context>
     <name>DbManager</name>
     <message>
+        <location filename="DbManager.cpp" line="23"/>
         <source>Cannot open database</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="DbManager.cpp" line="24"/>
         <source>Unable to establish a database connection.
 This example needs SQLite support. Please read the Qt SQL driver documentation for information how to build it.</source>
         <translation type="unfinished"></translation>
@@ -16,17 +18,95 @@ This example needs SQLite support. Please read the Qt SQL driver documentation f
 <context>
     <name>ListView</name>
     <message>
+        <location filename="ListView.cpp" line="28"/>
         <source>No Results</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="SettingsDialog.cpp" line="16"/>
+        <source>server.ip</source>
+        <oldsource>server.base_url</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="17"/>
+        <source>server.port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="18"/>
+        <source>server.owner</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="19"/>
+        <source>server.password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="21"/>
+        <source>Typora path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="31"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="44"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="45"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="66"/>
+        <source>Choose Typora Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="75"/>
+        <source>SettingsDialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="83"/>
+        <source>server.ip can&apos;t be empty</source>
+        <oldsource>server.base_url can&apos;t be empty</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="87"/>
+        <source>server.owner can&apos;t be empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="91"/>
+        <source>server.password can&apos;t be empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="96"/>
+        <source>Typora path can&apos;t be empty</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>ToastClass</name>
     <message>
+        <location filename="cmake-build-debug/MyNotes_autogen/include/ui_Toast.h" line="59"/>
         <source>Toast</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="cmake-build-debug/MyNotes_autogen/include/ui_Toast.h" line="60"/>
         <source>TextLabel</source>
         <translation type="unfinished"></translation>
     </message>
@@ -34,78 +114,105 @@ This example needs SQLite support. Please read the Qt SQL driver documentation f
 <context>
     <name>Widget</name>
     <message>
-        <source>New Note</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Note name</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>New Folder</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Folder name</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>trash note fail</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>trash folder fail</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Trash Note</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Trash Note?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>File</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>File not exist.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>File open fail.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>current item is null</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>ERROR</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>folder exist.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>new folder: save to db fail</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
+        <location filename="Widget.cpp" line="82"/>
         <source>total 0 words</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="Widget.cpp" line="183"/>
+        <source>Add to ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="250"/>
+        <source>Trash Note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="251"/>
+        <source>Trash Note?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="328"/>
+        <source>New Note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="328"/>
+        <source>Note name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="341"/>
+        <location filename="Widget.cpp" line="388"/>
+        <source>current item is null</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="374"/>
+        <source>New Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="374"/>
+        <source>Folder name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="394"/>
+        <source>folder exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="401"/>
+        <source>new folder: save to db fail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="414"/>
+        <location filename="Widget.cpp" line="420"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="414"/>
+        <source>File not exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="420"/>
+        <source>File open fail.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="438"/>
+        <location filename="Widget.cpp" line="447"/>
+        <source>trash note fail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="463"/>
+        <source>trash folder fail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="477"/>
+        <source>ERROR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Widget.cpp" line="658"/>
         <source>open in typora fail</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="Widget.cpp" line="677"/>
         <source>export to HTML fail</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="Widget.cpp" line="682"/>
         <source>Export Note to HTML</source>
         <translation type="unfinished"></translation>
     </message>
