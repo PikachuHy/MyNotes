b1b3634adb5d8de0e4055128a42a8d418a5dcc8e
PikachuHy <pikachuhy@163.com>
2021-04-02 22:16:51 +0800

    格式化代码

--- SingleApplication.cpp
+++ SingleApplication.cpp
@@ -1,29 +1,28 @@
 //
 // Created by PikachuHy on 2021/4/2.
 //
+// https://blog.csdn.net/playstudy/article/details/7796691
+// 第二种实现方法
 
 #include "SingleApplication.h"
-#include <QLocalSocket>
 #include <QTimer>
 #include <QByteArray>
 
-SingleApplication::SingleApplication(int &argc, char *argv[], const QString& uniqueKey) : QApplication(argc, argv)
-{
+SingleApplication::SingleApplication(int &argc, char *argv[], const QString &uniqueKey)
+        : QApplication(argc, argv) {
     sharedMemory.setKey(uniqueKey);
     if (sharedMemory.attach())
         _isRunning = true;
-    else
-    {
+    else {
         _isRunning = false;
         // attach data to shared memory.
         QByteArray byteArray("0"); // default value to note that no message is available.
-        if (!sharedMemory.create(byteArray.size()))
-        {
+        if (!sharedMemory.create(byteArray.size())) {
             qDebug("Unable to create single instance.");
             return;
         }
         sharedMemory.lock();
-        char *to = (char*)sharedMemory.data();
+        char *to = (char *) sharedMemory.data();
         const char *from = byteArray.data();
         memcpy(to, from, qMin(sharedMemory.size(), byteArray.size()));
         sharedMemory.unlock();
@@ -36,10 +35,9 @@ SingleApplication::SingleApplication(int &argc, char *argv[], const QString& uni
 }
 
 // public slots.
-void SingleApplication::checkForMessage()
-{
+void SingleApplication::checkForMessage() {
     sharedMemory.lock();
-    QByteArray byteArray = QByteArray((char*)sharedMemory.constData(), sharedMemory.size());
+    QByteArray byteArray = QByteArray((char *) sharedMemory.constData(), sharedMemory.size());
     sharedMemory.unlock();
     if (byteArray.left(1) == "0")
         return;
@@ -50,20 +48,18 @@ void SingleApplication::checkForMessage()
     // remove message from shared memory.
     byteArray = "0";
     sharedMemory.lock();
-    char *to = (char*)sharedMemory.data();
+    char *to = (char *) sharedMemory.data();
     const char *from = byteArray.data();
     memcpy(to, from, qMin(sharedMemory.size(), byteArray.size()));
     sharedMemory.unlock();
 }
 
 // public functions.
-bool SingleApplication::isRunning() const
-{
+bool SingleApplication::isRunning() const {
     return _isRunning;
 }
 
-bool SingleApplication::sendMessage(const QString &message)
-{
+bool SingleApplication::sendMessage(const QString &message) {
     if (!_isRunning)
         return false;
 
@@ -71,7 +67,7 @@ bool SingleApplication::sendMessage(const QString &message)
     byteArray.append(message.toUtf8());
     byteArray.append('0'); // < should be as char here, not a string!
     sharedMemory.lock();
-    char *to = (char*)sharedMemory.data();
+    char *to = (char *) sharedMemory.data();
     const char *from = byteArray.data();
     memcpy(to, from, qMin(sharedMemory.size(), byteArray.size()));
     sharedMemory.unlock();
--- SingleApplication.h
+++ SingleApplication.h
@@ -12,22 +12,27 @@
 #include <QApplication>
 #include <QSharedMemory>
 
-class SingleApplication : public QApplication
-{
+class SingleApplication : public QApplication {
 Q_OBJECT
 public:
-    SingleApplication(int &argc, char *argv[], const QString& uniqueKey);
+    SingleApplication(int &argc, char *argv[], const QString &uniqueKey);
+
     bool isRunning() const;
+
     bool sendMessage(const QString &message);
+
 public slots:
+
     void checkForMessage();
+
 signals:
-    void messageAvailable(QString& message);
+
+    void messageAvailable(QString &message);
+
 private:
     bool _isRunning;
     QSharedMemory sharedMemory;
 };
-
 
 
 #endif //MYNOTES_SINGLEAPPLICATION_H
