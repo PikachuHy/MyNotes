b95ef2ad63080f39f61bdab071174c7cf6a39c3e
PikachuHy <pikachuhy@163.com>
2021-04-04 13:08:34 +0800

    修改监控文件夹中图片的位置为相对位置

--- Widget.cpp
+++ Widget.cpp
@@ -1101,7 +1101,145 @@ void Widget::syncWatchingFolder(const QString &path) {
     }
 
 }
-
+struct WatchingFileHtmlVisitor: MultipleVisitor<Header,
+        Text, ItalicText, BoldText, ItalicBoldText,
+        Image, Link, CodeBlock, InlineCode, Paragraph,
+        UnorderedList, OrderedList,
+        Hr, QuoteBlock, Table> {
+    void visit(Header *node) override {
+        auto hn = "h" + String::number(node->level());
+        m_html += "<" + hn + ">";
+        for(auto it: node->children()) {
+            it->accept(this);
+        }
+        m_html += "</" + hn + ">\n";
+    }
+    void visit(Text *node) override {
+        m_html += node->str();
+    }
+    void visit(ItalicText *node) override {
+        m_html += "<em>" + node->str() + "</em>";
+    }
+    void visit(BoldText *node) override {
+        m_html += "<strong>" + node->str() + "</strong>";
+    }
+    void visit(ItalicBoldText *node) override {
+        m_html += "<strong><em>" + node->str() + "</strong></em>";
+    }
+    void visit(Image *node) override {
+        m_html += R"(<img alt=")";
+        if (node->alt()) {
+            node->alt()->accept(this);
+        } else {
+            qDebug() << "image alt is null";
+        }
+        m_html += R"(" src=")";
+        if(node->src()) {
+            QString path = node->src()->str();
+            QFileInfo info(path);
+            if (info.exists() && info.isFile()) {
+                m_html += info.fileName();
+                m_pathList.append(path);
+            }
+        } else {
+            qDebug() << "image src is null";
+        }
+        m_html += R"(" />)";
+        m_html += "\n";
+    }
+    void visit(Link *node) override {
+        m_html += R"(<a href=")";
+        if (node->href()) {
+            node->href()->accept(this);
+        } else {
+            qDebug() << "link href is null";
+        }
+        m_html += R"(">)";
+        if(node->content()) {
+            node->content()->accept(this);
+        } else {
+            qDebug() << "link content is null";
+        }
+        m_html += R"(</a>)";
+        m_html += "\n";
+    }
+    void visit(CodeBlock *node) override {
+        m_html += "<pre><code>\n";
+        auto code = node->code()->str();
+        m_html += code.toHtmlEscaped();
+        m_html += "</code></pre>\n";
+    }
+    void visit(InlineCode *node) override {
+        m_html += "<code>";
+        if (auto code = node->code(); code) {
+            code->accept(this);
+        }
+        m_html += "</code>\n";
+    }
+    void visit(Paragraph *node) override {
+        m_html += "<p>";
+        for(auto it: node->children()) {
+            it->accept(this);
+        }
+        m_html += "</p>\n";
+    }
+    void visit(UnorderedList *node) override {
+        m_html += "<ul>\n";
+        for(auto it: node->children()) {
+            m_html += "\t<li>";
+            it->accept(this);
+            m_html += "</li>\n";
+        }
+        m_html += "</ul>\n";
+    }
+    void visit(OrderedList *node) override {
+        m_html += "<ol>\n";
+        for(auto it: node->children()) {
+            m_html += "\t<li>";
+            it->accept(this);
+            m_html += "</li>\n";
+        }
+        m_html += "</ol>\n";
+    }
+    void visit(Hr *node) override {
+        m_html += "<hr/>\n";
+    }
+    void visit(QuoteBlock *node) override {
+        m_html += "<blockquote>\n";
+        for(auto it: node->children()) {
+            it->accept(this);
+            m_html += "\n";
+        }
+        m_html += "</blockquote>\n";
+    }
+    void visit(Table *node) override {
+        m_html += "<table>\n";
+        m_html += "<thead><tr>";
+        for(const auto &content: node->header()) {
+            m_html += "<th>";
+            m_html += content;
+            m_html += "</th>";
+        }
+        m_html += "</tr></thead>\n";
+        m_html += "<tbody>\n";
+        for(const auto & row: node->content()) {
+            m_html += "<tr>";
+            for(const auto & content: row) {
+                m_html += "<th>";
+                m_html += content;
+                m_html += "</th>";
+            }
+            m_html += "</tr>";
+        }
+        m_html += "</tbody>";
+        m_html += "</table>\n";
+    }
+    String html() { return m_html; }
+    QStringList pathList() { return m_pathList; }
+private:
+    String m_html;
+    QStringList m_pathList;
+};
 void Widget::syncWatchingFile(const QString& path) {
     qInfo() << "sync watching file:" << path;
     auto title = QFileInfo(path).baseName();
@@ -1109,7 +1247,10 @@ void Widget::syncWatchingFile(const QString& path) {
     mdFile.open(QIODevice::ReadOnly);
     Document doc(mdFile.readAll());
     mdFile.close();
-    auto html = doc.toHtml();
+    WatchingFileHtmlVisitor visitor;
+    doc.accept(&visitor);
+    auto html = visitor.html();
+    auto pathList = visitor.pathList();
     QString owner = Settings::instance()->usernameEn;
     ServerNoteInfo info;
     info.title = title;
@@ -1117,6 +1258,7 @@ void Widget::syncWatchingFile(const QString& path) {
     info.noteHtml = html;
     info.strId = Utils::md5(path);
     m_esApi->putNote(info);
+
 }
 
 void Widget::showSyncResult(const QString& msg) {
